dbd3200c0a6a7787584e4c151677c5f4cb2faa14
Jack Gronenthal <jlg6@illinois.edu>
2020-04-09 00:40:13 -0500

    Updates

---
+++ precache-manifest.22b61060bcd89368e934dc48278a4081.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb958198c3c7a2de9a726355c5db8d5",
+    "url": "/crowddit%20/index.html"
+  },
+  {
+    "revision": "b944433571ed5f24e7c0",
+    "url": "/crowddit%20/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "017717646aca1b9b67d1",
+    "url": "/crowddit%20/static/css/main.df1bad64.chunk.css"
+  },
+  {
+    "revision": "b944433571ed5f24e7c0",
+    "url": "/crowddit%20/static/js/2.7311c5ac.chunk.js"
+  },
+  {
+    "revision": "6fea5a45bbb56827f9f82c4c74c29929",
+    "url": "/crowddit%20/static/js/2.7311c5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017717646aca1b9b67d1",
+    "url": "/crowddit%20/static/js/main.c1112c55.chunk.js"
+  },
+  {
+    "revision": "ae10891d354d48bf3a7d",
+    "url": "/crowddit%20/static/js/runtime-main.31c69d83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crowddit%20/static/media/logo.5d5d9eef.svg"
+  }
+]);
