3913c73dbf76f6d2dc61730bea52b09a99b209e1
Cheng <chenghang8888@gmail.com>
2020-04-09 20:56:22 -0500

    Added routes.

--- api/routes/reddit.js
+++ api/routes/reddit.js
@@ -8,17 +8,30 @@ const sw = new snoowrap({
     userAgent: 'crowddit',
     clientId: 'DBZ2ZJ37Bs431A',
     clientSecret: 'J_-QRQAYKNPe24Je-QiiAxk7QYM',
-    refreshToken: '481153194914-AAkuEVOU_MLIpmbJYyiiDszIdus',
+    refreshToken: '481153194914-pY86yVBFfVLOPEz_ImS9HV4Sy7Y',
 })
  
 router.get('/savedposts', (request, response, next) => {
-    sw.getMe().getSavedContent().then(post => {
+    sw.getMe().getSavedContent().then(posts => {
         response.status(200).json({
             status: "success",
             message: "Saved posts retrieved.",
-            post
+            posts
         });
     });
+
+})
+
+router.get('/subreddits', (request, response, next) => {
+    // sw.getSubscriptions().then(subreddits => {
+    //     response.status(200).json({
+    //         status: "success",
+    //         message: "Subscribed subreddits retrieved.",
+    //         subreddits
+    //     });
+    // });
+
+    sw.getSubscriptions().then(console.log);
 
 })
 
@@ -30,7 +43,7 @@ router.get('/auth', (request, response, next) => {
         response_type: 'code',
         state,
         redirect_uri: 'https://indivism.github.io/crowddit',
-        duration: 'permanent',
+        permanent: true,
         scope: 'history identity mysubreddits read save subscribe'
     }
 
@@ -38,7 +51,7 @@ router.get('/auth', (request, response, next) => {
 
     response.status(200).json({
         status: "success",
-        message: "Saved posts retrieved.",
+        message: "URL generated.",
         urlString
     });
 })
