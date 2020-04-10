19fe5ecb89aefc7ea396de33bbca8969e18dbd97
Cheng <chenghang8888@gmail.com>
2020-04-09 23:17:48 -0500

    Logging

--- api/routes/reddit.js
+++ api/routes/reddit.js
@@ -52,6 +52,7 @@ router.get('/auth/callback', (request, response) => {
     fetch('https://www.reddit.com/api/v1/access_token', options)
     .then(res => res.json())
     .then(async json => {
+        console.log(crowddit, json.access_token, json.refresh_token)
         await db.insertTokenInformation(crowddit, json.access_token, json.refresh_token)
         response.status(200).json(json)
     })
